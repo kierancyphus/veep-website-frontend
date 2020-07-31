017eb8acaa439d3e1c9bbf602dec22c5f84fcfb9
kierancyphus <kcyphus@cwretirement.com>
2020-07-31 15:51:09 -0700

    basic react app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+import './App.scss'
+
+const App: React.StatelessComponent<{}>  = () => (
+    <div className='container'>
+        <h1>Hello World, React!</h1>
+    </div>
+)
+
+export default App;
